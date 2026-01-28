c51f04e754e9167cba14383f585804463ae9cf2c
Elena <elenaizmailova067@gmail.com>
2026-01-28 16:22:05 +0500

    change main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from platform import android_ver
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -6,6 +9,18 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def rate_lecture(self, lecturer, course, grade):
+        # Проверяем: лектор ли это, закреплен ли он за курсом и изучает ли студент этот курс
+        if (isinstance(lecturer, Lecturer) and
+            course in lecturer.courses_attached and
+            course in self.courses_in_progress):
+
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -15,7 +30,10 @@ class Mentor:
 
 # Класс лекторов наследуется от Mentor
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname) # Наследуем имя и фамилию
+        self.grades = {} # Добавляем словарь для оценок лектору
+
 # Класс экспертов наследуется от Mentor
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -27,23 +45,18 @@ class Reviewer(Mentor):
                     student.grades[course] = [grade]
             else:
                 return 'Ошибка'
-# Проверка реализации
+# --- Тестирование ---
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
 
-# Создаем эксперта (Reviewer)
-some_reviewer = Reviewer('Some', 'Buddy')
-some_reviewer.courses_attached += ['Python']
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
 
-# Создаем лектора (Lecturer)
-some_lecturer = Lecturer('Ivan', 'Ivanov')
-some_lecturer.courses_attached += ['Python']
+print(student.rate_lecture(lecturer, 'Python', 7))   # None (успешно)
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка (лектор не ведет Java)
+print(student.rate_lecture(lecturer, 'C++', 8))      # Ошибка (студент не учит C++)
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка (ревьюера нельзя оценивать)
 
-# Создаем студента
-some_student = Student('Ruoy', 'Eman', 'm')
-some_student.courses_in_progress += ['Python']
-
-# Проверяющий выставляет оценку
-some_reviewer.rate_hw(some_student, 'Python', 10)
-
-print(f"Имя ревьюера: {some_reviewer.name}")
-print(f"Имя лектора: {some_lecturer.name}")
-print(f"Оценки студента: {some_student.grades}")
+print(lecturer.grades)  # {'Python': [7]}
